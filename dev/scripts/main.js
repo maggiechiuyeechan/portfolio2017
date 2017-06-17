c4934303cc4ba9c67626a6932d5e9c59deadf40f
Maggie Chan <mach.sq@gmail.com>
2017-06-17 11:40:51 -0400

    Portfolio initial commit

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,48 @@
-const ohHey = "Hello World";
+const portfolioSite = {};
 
-console.log(ohHey);
+portfolioSite.isotopeFeatures =  function(){ 
+
+	$('.carousel').flickity({
+	  // options
+	  cellAlign: 'left',
+	  contain: true,
+	  imagesLoaded:true,
+	  perscentPosition: false,
+	  autoPlay: 2500,
+	  wrapAround: true,
+	  prevNextButtons: false
+	});
+
+	var $grid = $('.grid').imagesLoaded(
+		function(){
+				$grid.isotope({
+					percentPosition: true, 
+					layoutMode: 'masonry',
+					stagger: 30,
+					transitionDuration: '0.2s'
+				});
+			});
+
+	$('.filter-button-group').on( 'click', 'button', function() {
+		var filterValue = $(this).attr('data-filter');
+		$grid.isotope({ filter: filterValue });
+	});
+}
+
+//////////////////////
+//////////////////////
+//INIT AND DOC READY//
+//////////////////////
+//////////////////////
+
+portfolioSite.init = function(){
+	console.log("hello!");
+};
+
+$(function() {
+	//This is our starting point. When the DOM is ready we call the init
+	//method to start things off.
+	portfolioSite.init(); 
+	portfolioSite.isotopeFeatures();
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,51 @@
-const gulp = require("gulp"); //require gulp and all other gulp packages
-const sass = require("gulp-sass");
-const concat = require("gulp-concat");
-const babel = require("gulp-babel");
-const browserSync = require("browser-sync").create();
+// gulpfile.js
+const gulp = require('gulp');
+const babel = require('babelify');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const browserSync = require('browser-sync');
 const reload = browserSync.reload;
+const notify = require('gulp-notify');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const concat = require('gulp-concat');
 
-gulp.task("browser-sync", () => {
-	browserSync.init({
-		server: "."
-	})
+gulp.task('styles', () => {
+    return gulp.src('./dev/styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('./public/styles'))
 });
 
-
-gulp.task("styles", () => {
-	return gulp.src("./dev/styles/**/**") //*globbing* pattern - look in all folders for files with .scss 
-		.pipe(sass().on("error", sass.logError)) //listen for any errors 
-		.pipe(concat("styles.css"))
-		.pipe(gulp.dest("./public/styles"))
-		.pipe(reload({stream:true}));
-}); 
-
-gulp.task("scripts", () => {
-	return gulp.src("./dev/scripts/main.js")
-		.pipe(babel({
-			presets: ["es2015"]
-		}))
-		.pipe(gulp.dest("./public/scripts"))
-		.pipe(reload({stream:true}));
+gulp.task('js', () => {
+    browserify('dev/scripts/main.js', {debug: true})
+        .transform('babelify', {
+            sourceMaps: true,
+            presets: ['es2015']
+        })
+        .bundle()
+        .on('error',notify.onError({
+            message: "Error: <%= error.message %>",
+            title: 'Error in JS 💀'
+        }))
+        .pipe(source('main.js'))
+        .pipe(buffer())
+        .pipe(gulp.dest('public/scripts'))
+        .pipe(reload({stream:true}));
 });
 
-
-gulp.task("watch", () => {
-	gulp.watch("./dev/styles/**/*.scss", ["styles"]);
-	gulp.watch("./dev/scripts/main.js", ["scripts"]);
-	gulp.watch("*.html", reload);
-	 //array of what you want to execute when watching
+gulp.task('bs', () => {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
 });
 
-gulp.task("default", ["browser-sync", "styles", "scripts", "watch"]);
+gulp.task('default', ['js','bs', 'styles'], () => {
+    gulp.watch('dev/**/*.js',['js']);
+    gulp.watch('dev/**/*.scss',['styles']);
+    gulp.watch('*.html', reload);
+    gulp.watch('./public/styles/styles.css',reload);
+});
